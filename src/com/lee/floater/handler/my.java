b2d33ee0a529a8d5f4206a8adb273e11e2a6f871
ZJJACL <1152512286@qq.com>
2016-03-18 09:05:50 +0800

    jie

--- src/com/lee/floater/handler/my.java
+++ src/com/lee/floater/handler/my.java
@@ -3,8 +3,8 @@ package com.lee.floater.handler;
 public class my {
 
    public int i=0;
-  public char h='j';
-  public void t(){
+    public String k=null;
+    public void t(){
     	
     	
     }
